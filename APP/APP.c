6488f52713033d6dc7d9dd16da6282e37a6d4fd7
Abdelmageed23 <abd.abdelmageed@gmail.com>
2022-01-29 15:05:26 +0200

    Control Traffic Light by UART

--- APP/APP.c
+++ APP/APP.c
@@ -23,46 +23,46 @@ void APP_Init()
 
 void APP_Run()
 {
-			/* Initializing Variable To store The Command*/
-			uint8_t local_TempStr[MAX_SIZE];
+	/* Initializing Variable To store The Command*/
+	uint8_t local_TempStr[MAX_SIZE];
 
-				/*Asking for the Command*/
-				UART_SendString("Enter The Command: ");
-				/*Getting the Command and  confirm it*/
-		    	UART_ReceiveString(local_TempStr);
-		    	UART_SendString(local_TempStr);
-		    	UART_SendString("\r\n");
+		/*Asking for the Command*/
+		UART_SendString("Enter The Command: ");
+		/*Getting the Command and  confirm it*/
+		UART_ReceiveString(local_TempStr);
+		UART_SendString(local_TempStr);
+		UART_SendString("\r\n");
 
-		    	/* Check if the command match the condtions*/
-		    	if(strcmp(local_TempStr,"START")==IDINTICAL)
-		    	{
-		    		LED_ON(GREEN);
-		    		LED_OFF(YELLOW);
-		    		LED_OFF(RED);
-		    		UART_SendString("Green LED is on \r\n");
-		    	}
-		    	else if(strcmp(local_TempStr,"WAIT")==IDINTICAL)
-		    	{
-		    		LED_OFF(GREEN);
-		    		LED_ON(YELLOW);
-					LED_OFF(RED);
-					UART_SendString("Yellow LED is on\r\n");
-		    	}
-		    	else if(strcmp(local_TempStr,"STOP")==IDINTICAL)
-		    	{
-		    		LED_OFF(GREEN);
-		    		LED_OFF(YELLOW);
-					LED_ON(RED);
-					UART_SendString("Red LED is on \r\n");
-		    	}
-		    	else if(strcmp(local_TempStr,"AT")==IDINTICAL)
-		    	{
-					UART_SendString("OK \r\n");
-		    	}
-		    	else
-		    	{
-		    		UART_SendString(local_TempStr);
-		    		UART_SendString("\r\n");
-		    	}
+		/* Check if the command match the condtions*/
+		if(strcmp(local_TempStr,"START")==IDINTICAL)
+		{
+			LED_ON(GREEN);
+			LED_OFF(YELLOW);
+			LED_OFF(RED);
+			UART_SendString("Green LED is on \r\n");
+		}
+		else if(strcmp(local_TempStr,"WAIT")==IDINTICAL)
+		{
+			LED_OFF(GREEN);
+			LED_ON(YELLOW);
+			LED_OFF(RED);
+			UART_SendString("Yellow LED is on\r\n");
+		}
+		else if(strcmp(local_TempStr,"STOP")==IDINTICAL)
+		{
+			LED_OFF(GREEN);
+			LED_OFF(YELLOW);
+			LED_ON(RED);
+			UART_SendString("Red LED is on \r\n");
+		}
+		else if(strcmp(local_TempStr,"AT")==IDINTICAL)
+		{
+			UART_SendString("OK \r\n");
+		}
+		else
+		{
+			UART_SendString(local_TempStr);
+			UART_SendString("\r\n");
+		}
 
 }
